42b2d0253f36ca2a00a5c9ff624ae5b717329165
RigobertoPO <rperez@cobach.edu.mx>
2021-06-18 23:34:06 -0500

    librerias bootstrap

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Alumnos POO</title>
 </head>
 <body>
-
     <h1>Lista de alumnos</h1>
     <?php
     require_once 'Conexion.php';
